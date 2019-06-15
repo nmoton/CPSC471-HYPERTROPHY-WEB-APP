e1452d0081e01a7e3f05508389c46f6702c53534
nmoton <26640786+nmoton@users.noreply.github.com>
2019-06-15 02:36:29 -0600

    Updated workoutListHandler to change outputs depending on workout privacy

--- pages/dashboard/workoutListHandler.php
+++ pages/dashboard/workoutListHandler.php
@@ -1,29 +1,31 @@
 <?php
 $db = mysqli_connect('localhost', 'root', '', '471proj');
 
-$searchQuery = "SELECT * FROM workout WHERE workoutListID_fk = '1'";
+$searchQuery = "SELECT * FROM workout WHERE workoutListID_fk = '1' ORDER BY workoutID DESC";
 $result = mysqli_query($db, $searchQuery);
 
-$i = 1;
+$num_workouts = mysqli_num_rows($result);
 
 while ($row = mysqli_fetch_assoc($result)){
-		echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">';
+	if ($row['privacy'] == 'public'){
+		echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start list-group-item-primary">';
 	    	echo '<div class="d-flex justify-content-between" id="workout">';
-	        	echo '<h5 class="mb-1">Workout #' . $i . '</h5>';
+	        	echo '<h5 class="mb-1">Workout #' . $num_workouts . '</h5>';
 	        	echo '<small>' . $row['wMonth'] . '/' .$row['wDay'] . '/' . $row['wYear'] . '</small>';
 	    	echo '</div>';
 	    	echo '<p class="mb-1">Workout Description</p>';
-
-	    	if ($row['privacy'] == 'public'){
-	    		echo '<small> Private - Stored only in your workout list</small>';
-	    	}
-
-	    	else {
-	    		echo '<small>Public - Shared on your personal wall and the community wall</small>';
-	    	}
-	    	
-	    echo' </a>';
-
-	    $i++;
+			echo '<small>Public - Shared on your personal wall and the community wall</small>';
+			echo' </a>';
+	} else {
+		echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">';
+	    	echo '<div class="d-flex justify-content-between" id="workout">';
+	        	echo '<h5 class="mb-1">Workout #' . $num_workouts . '</h5>';
+	        	echo '<small>' . $row['wMonth'] . '/' .$row['wDay'] . '/' . $row['wYear'] . '</small>';
+	    	echo '</div>';
+	    	echo '<p class="mb-1">Workout Description</p>';
+	    	echo '<small> Private - Stored only in your workout list</small>';
+	    	echo' </a>';
 	}
+	$num_workouts--;
+}
 ?>
